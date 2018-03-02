9ce1c1af0f55fd72c64f1b2e3e2e92ee9bbdb940
Alexis Métaireau <alexis@notmyidea.org>
2018-03-02 10:24:55 +0100

    Let the script handle the values update

--- theme/static/js/countdown.js
+++ theme/static/js/countdown.js
@@ -1,22 +1,31 @@
-CountDownTimer('05/15/2018 10:1 AM', 'countdown');
-function CountDownTimer(dt, id)
+CountDownTimer('05/15/2018 10:1 AM', 'countdown', 20);
+function CountDownTimer(date, id, liters)
 {
-    var end = new Date(dt);
-
+    var end = new Date(date);
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
     var _day = _hour * 24;
-    var timer;
 
     function showRemaining() {
-        var countdown = document.getElementById(id);
+        // Update the progressbar.
+        var progressElement = document.getElementById('progress');
+        var volumeElement = document.getElementById('current-volume');
+        var percentageElement = document.getElementById('current-percentage');
+
+        percentage = (liters / 2000) * 100;
+        var percentageText = percentage + '%';
+        progressElement.style.width = percentageText;
+        percentageElement.innerHTML = percentage;
+        volumeElement.innerHTML = liters;
+
+        // Handle the date update.
         var now = new Date();
         var distance = end - now;
         if (distance < 0) {
 
             clearInterval(timer);
-            if (countdown) countdown.innerHTML = '0';
+            countdown.innerHTML = '0';
 
             return;
         }
@@ -25,11 +34,15 @@ function CountDownTimer(dt, id)
         var minutes = Math.floor((distance % _hour) / _minute);
         var seconds = Math.floor((distance % _minute) / _second);
 
-        if (countdown) countdown.innerHTML = days /*+ ' days'*/;
+        countdown.innerHTML = days /*+ ' days'*/;
         //document.getElementById(id).innerHTML += hours + 'hrs ';
         //document.getElementById(id).innerHTML += minutes + 'mins ';
         //document.getElementById(id).innerHTML += seconds + 'secs';
     }
 
-    timer = setInterval(showRemaining, 1000);
+    var timer;
+    var countdown = document.getElementById(id);
+    if (countdown) {
+      timer = setInterval(showRemaining, 1000);
+    }
 }
