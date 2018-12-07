686d28264699a954d50b22eb4174522d238d50dd
Alexis Métaireau <alexis@vieuxsinge.com>
2018-12-07 20:11:54 +0100

    Set to http for now

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # NoQA
 
-SITEURL = 'https://www.vieuxsinge.com'
+SITEURL = 'http://www.vieuxsinge.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
