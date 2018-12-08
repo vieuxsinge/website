c42908ddd50937f9ead60f80f562d05364bca59f
Alexis Métaireau <alexis@vieuxsinge.com>
2018-12-08 11:08:48 +0100

    Revert back to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # NoQA
 
-SITEURL = 'http://www.vieuxsinge.com'
+SITEURL = 'https://www.vieuxsinge.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
