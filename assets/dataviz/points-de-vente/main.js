71e2fc6fbc80d8d3a5c1116b70091a6038dbb7c0
Travis CI User <travis@example.org>
2021-05-10 14:26:43 +0000

    Update documentation

---
+++ assets/dataviz/points-de-vente/main.js
@@ -0,0 +1,29 @@
+var map = L.map('map').setView([46.6, 2.1], 6);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Fonds © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox.light'
+}).addTo(map);
+
+var placesLayer = omnivore.csv('assets/dataviz/points-de-vente/geocoded.csv', {
+  latfield: 'latitude',
+  lonfield: 'longitude',
+  delimiter: ';'});
+
+placesLayer.on('ready', function() {
+  map.fitBounds(placesLayer.getBounds());
+  placesLayer.eachLayer(function(layer) {
+    layer.bindPopup("<strong>" + layer.feature.properties.Nom + "</strong>" + "<br />" + layer.feature.properties.Adresse);
+  });
+});
+
+placesLayer.on('error', function(e) {
+  console.log("Oops, something went wrong.", e)
+});
+
+placesLayer.addTo(map);
+
+// Hide the controls from leaflet. We don't need them here.
+var lc = document.getElementsByClassName('leaflet-control-zoom');
+lc[0].style.visibility = 'hidden';
---
+++ theme/js/countdown.js
@@ -0,0 +1,63 @@
+
+// CountDownTimer('05/05/2018 00:00 AM', 'countdown');
+
+function httpGetAsync(url, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
+
+function CountDownTimer(date, id)
+{
+    var end = new Date(date);
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+
+    function showRemaining(liters) {
+        // Update the progressbar.
+        var progressElement = document.getElementById('progress');
+        var volumeElement = document.getElementById('current-volume');
+        var percentageElement = document.getElementById('current-percentage');
+
+        percentage = (liters / 2000) * 100;
+        var percentageText = Math.round(percentage * 100) / 100 + '%';
+        progressElement.style.width = percentageText;
+        percentageElement.innerHTML = percentageText;
+        volumeElement.innerHTML = liters + ' litres';
+
+        // Handle the date update.
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            countdown.innerHTML = '0';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        countdown.innerHTML = days /*+ ' days'*/;
+        //document.getElementById(id).innerHTML += hours + 'hrs ';
+        //document.getElementById(id).innerHTML += minutes + 'mins ';
+        //document.getElementById(id).innerHTML += seconds + 'secs';
+    }
+
+    var timer;
+    var countdown = document.getElementById(id);
+    if (countdown) {
+      httpGetAsync('https://kinto.notmyidea.org/v1/buckets/vieuxsinge/collections/preventes/records/7818e391-47bc-4e53-bb92-6cf57da612b9', function(data) {
+        showRemaining(JSON.parse(data)['data']['liters']);
+      });
+    }
+}
